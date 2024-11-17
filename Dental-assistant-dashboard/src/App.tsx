403650d1c74c0192fb6fb0ecccab2944454489f7
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-11-16 20:32:26 -0500

    edited app.tsx

--- Dental-assistant-dashboard/src/App.tsx
+++ Dental-assistant-dashboard/src/App.tsx
@@ -1,19 +1,10 @@
 import React from "react";
-import TopBar from "./components/TopBar";
-import Footer from "./components/footer";
-import SideBar from "./components/SideBar";
+import Layout from "./components/Layout";
 
-const App = () => {
+function App() {
   return (
-    <div id="root" className="flex flex-col min-h-screen pb-12">
-      <TopBar />
-      <main className="main-content flex-1 p-8 mt-[3rem]">
-        {/* Main content goes here */}
-        <SideBar />
-      </main>
-      <Footer />
-    </div>
+    <Layout />
   );
-};
+}
 
 export default App;
