b2a38523ea24f8af65ac446558c2c2c2ddbf84ab
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-11-18 16:27:48 -0500

    minor change in footer

--- Dental-assistant-dashboard/src/components/footer.tsx
+++ Dental-assistant-dashboard/src/components/footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     // wee need to work on the footer navbar. it is ok by now but need to work on it
 
-    <footer className="bg-light text-dark py-6 fixed bottom-0 w-full">
+    <footer className="bg-light text-dark py-6 fixed bottom-0 w-4/5">
       {/* it needs some changes in here => items-center pr-[300px] */}
       <div className="container mx-auto text-center flex justify-center items-center ">
         {/* Footer Links */}
